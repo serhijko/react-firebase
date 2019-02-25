8bd7f6f8bec2fdc2b68bfe51b1594c5470df07b9
serhijko <serhijko>
2019-02-25 21:06:59 +0300

    Pagination

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -31,7 +31,10 @@ class MessagesBase extends Component {
   componentDidMount() {
     this.setState({ loading: true });
 
-    this.props.firebase.messages().on('value', snapshot => {
+    this.props.firebase
+      .messages()
+      .orderByChild('createdAt')
+      .on('value', snapshot => {
       const messageObject = snapshot.val();
 
       if (messageObject) {
